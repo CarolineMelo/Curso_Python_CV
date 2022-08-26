b560780363764a5ec97d94e14b36bc3b105788ae
Caroline <2015302@aluno.univesp.br>
2022-08-25 23:20:37 -0300

    aula07

--- teste.py
+++ teste.py
@@ -7,7 +7,13 @@
 # print ('Você nasceu no dia '+dia+' de '+mes+' de '+ano+'. Correto?')
 
 # =====Desafio 03======
-number1= int(input('Digite o primeiro número: '))
-number2= int(input('Digite o segundo número: '))
-soma = number1 + number2
-print('A soma é {}'.format(soma))
+# number1= int(input('Digite o primeiro número: '))
+# number2= int(input('Digite o segundo número: '))
+# soma = number1 + number2
+# print('A soma é {}'.format(soma))
+
+
+
+n1= int(input('Digite um número: '))
+n2= int(input('Digite outro: '))
+print('A soma é {}'.format(n1+n2))
