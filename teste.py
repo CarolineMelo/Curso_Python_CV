a208f6133919ca4795e6e6c4e9c723d01d05a566
Caroline <2015302@aluno.univesp.br>
2022-08-25 23:03:11 -0300

    Desfaio 03

--- teste.py
+++ teste.py
@@ -1,7 +1,13 @@
-nome = input ('Qual é o seu nome?')
-print ('Olá '+nome+'! Prazer em te conhecer.')
+# nome = input ('Qual é o seu nome?')
+# print('Olá '+nome+'! Prazer em te conhecer.')
 
-dia = input ('Dia= ')
-mes = input ('Mês= ')
-ano = input ('Ano= ')
-print ('Você nasceu no dia '+dia+' de '+mes+' de '+ano+'. Correto?')
+# dia = input ('Dia= ')
+# mes = input ('Mês= ')
+# ano = input ('Ano= ')
+# print ('Você nasceu no dia '+dia+' de '+mes+' de '+ano+'. Correto?')
+
+# =====Desafio 03======
+number1= int(input('Digite o primeiro número: '))
+number2= int(input('Digite o segundo número: '))
+soma = number1 + number2
+print('A soma é {}'.format(soma))
