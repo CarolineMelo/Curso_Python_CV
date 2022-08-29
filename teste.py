f03d55410bf7ed9000d10719e917bc61487eaa32
Caroline <2015302@aluno.univesp.br>
2022-08-29 17:23:46 -0300

    pythonteste

--- teste.py
+++ teste.py
@@ -14,6 +14,8 @@
 
 
 
-n1= int(input('Digite um número: '))
-n2= int(input('Digite outro: '))
-print('A soma é {}'.format(n1+n2))
+# n1= int(input('Digite um número: '))
+# n2= int(input('Digite outro: '))
+# print('A soma é {}'.format(n1+n2))
+
+
