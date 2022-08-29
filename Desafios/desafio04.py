5c26281afdd715fb57bdab39a1a30f1ec9fe2326
Caroline <2015302@aluno.univesp.br>
2022-08-29 17:50:46 -0300

    desafio04

--- Desafios/desafio04.py
+++ Desafios/desafio04.py
@@ -1,2 +1,7 @@
 #Mostrar na tela o seu tipo primitivo e todas as informações possíveis sobre ele.
 
+x = input('Digite algo: ')
+print(type(x))
+print(x.isalpha())
+print(x.isupper())
+print(x.isnumeric())
---
+++ PythonExercicios/ex003.py
@@ -0,0 +1,4 @@
+number1 = int(input('Digite o primeiro número: '))
+number2 = int(input('Digite o segundo número: '))
+s = number1 + number2
+print('A soma de {} mais {} é igual a {}!'.format(number1,number2,s))
