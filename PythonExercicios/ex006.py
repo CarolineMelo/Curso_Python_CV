ed9a97a802fc2c63b8d3db4748765c21ecdede03
Caroline <2015302@aluno.univesp.br>
2022-08-30 10:45:10 -0300

    ex006

--- PythonExercicios/ex006.py
+++ PythonExercicios/ex006.py
@@ -1,3 +1,5 @@
 #Crie um algoritmo que leia um número e mostre o seu dobro,triplo e raiz quadrada.
 number = int(input('Digite um número: '))
 print('O número digitado é {}.\nO seu dobro é {}.\nO seu triplo é {}.\nE sua raiz quadrada é {:.2f}' .format(number,number*2,number*3,number **(1/2)))
+
+#pow(number,(1/2))
