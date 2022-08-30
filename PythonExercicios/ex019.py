286e9fa9c0fb0b3985315f18375f457a1bf5d01b
Caroline <2015302@aluno.univesp.br>
2022-08-30 17:16:38 -0300

    ex019

--- PythonExercicios/ex019.py
+++ PythonExercicios/ex019.py
@@ -1,11 +1,11 @@
 # Um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele,
 #lendo o nome deles e escrevendo o nome escolhido.
-import random
+from random import choice
 aluno1 = input('Nome do rimeiro aluno: ')
 aluno2 = input('Nome do segundo aluno: ')
 aluno3 = input('Nome do terceiro aluno: ')
 aluno4 = input('Nome do quarto aluno: ')
 
 lista = [aluno1, aluno2, aluno3, aluno4]
-escolhido = random.choice(lista)
+escolhido =choice(lista)
 print('O aluno escolhido foi {}'.format(escolhido))
