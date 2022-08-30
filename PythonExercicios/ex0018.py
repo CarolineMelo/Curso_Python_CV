75151469ff18690e46c730960dd3ba17f545fab1
Caroline <2015302@aluno.univesp.br>
2022-08-30 17:05:17 -0300

    ex018

---
+++ PythonExercicios/ex0018.py
@@ -0,0 +1,20 @@
+'''import math
+angulo = float(input('Digite o ângulo que você deseja:'))
+seno = math.sin(math.radians(angulo))
+cosseno = math.cos(math.radians(angulo))
+tangente = math.tan(math.radians(angulo))
+print('O ângulo de {} tem o SENO de {:.2f}'.format(angulo, seno))
+print('O ângulo de {} tem o COSSENO de {:.2f}'.format(angulo, cosseno))
+print('O ângulo de {} tem a TANGENTE de {:.2f}'.format(angulo,tangente))'''
+
+
+#OU 
+
+from math import radians,sin,cos,tan
+angulo = float(input('Digite o ângulo que você deseja:'))
+seno = sin(radians(angulo))
+cosseno = cos(radians(angulo))
+tangente = tan(radians(angulo))
+print('O ângulo de {} tem o SENO de {:.2f}'.format(angulo, seno))
+print('O ângulo de {} tem o COSSENO de {:.2f}'.format(angulo, cosseno))
+print('O ângulo de {} tem a TANGENTE de {:.2f}'.format(angulo,tangente))
