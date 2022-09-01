5f69d5f38a53d53786173495ac92ca44edc40262
Caroline <2015302@aluno.univesp.br>
2022-08-31 23:02:08 -0300

    ex021

---
+++ PythonExercicios/ex021.py
@@ -0,0 +1,25 @@
+import pygame
+
+pygame.init()
+pygame.mixer.music.load('ex021.mp3')
+pygame.mixer.music.play()
+input()
+pygame.event.wait()
+
+# Faça um programa em Python que abra e reproduza o áudio de um arquivo
+# ================================================================================
+# import pygame
+
+# # Inicializando o mixer PyGame
+# pygame.mixer.init()
+
+# # Iniciando o Pygame
+# pygame.init()
+
+# pygame.mixer.music.load('ex021.mp3')
+# pygame.mixer.music.play(loops=0, start=0.0)
+# pygame.event.wait()
+#========================================================================================
+
+# import playsound
+# playsound.playsound('ex021.mp3')
