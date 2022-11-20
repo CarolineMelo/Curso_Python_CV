885d9c90d04ce7f75fba3a3065d3f719e9f6130b
Caroline <2015302@aluno.univesp.br>
2022-11-19 22:54:25 -0300

    Exercício 64 – Tratando vários valores v1.0

--- PythonExercicios/ex064.py
+++ PythonExercicios/ex064.py
@@ -1,11 +1,13 @@
 # Exercício Python 64: Crie um programa que leia vários números inteiros pelo teclado. O programa só vai parar quando o usuário digitar o valor 999, que é a condição de parada. No final, mostre quantos números foram digitados e qual foi a soma entre eles (desconsiderando o flag).
 
-contador = -1
+contador = 0
 number= 0
 total = 0
-while number != 999:
-    total += number
-    contador +=1
+while True:
+    
     number = int(input('Digite um valor inteiro [999 para parar]: '))
-
+    if number == 999:
+        break
+    contador +=1
+    total += number
 print('Você digitou {} números e a soma entre eles foi {}'.format(contador,total))
