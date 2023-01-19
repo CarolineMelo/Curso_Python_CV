b4daf1055c98e8486c924a90e44bf1a1286c30e3
Caroline <2015302@aluno.univesp.br>
2023-01-19 18:45:08 -0300

    Exercício 79 – Valores únicos em uma Lista

--- PythonExercicios/ex079.py
+++ PythonExercicios/ex079.py
@@ -1,4 +1,5 @@
-valores = list()
+'''Exercício Python 079: Crie um programa onde o usuário possa digitar vários valores numéricos e cadastre-os em uma lista. Caso o número já exista lá dentro, ele não será adicionado. No final, serão exibidos todos os valores únicos digitados, em ordem crescente.'''
+valores = []
 while True:
     valor = int(input('Digite um valor: '))
     if valor not in valores:
