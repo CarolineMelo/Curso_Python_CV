8e71c664f5473d73b63b42c8cc79e125c5574a0e
Caroline <2015302@aluno.univesp.br>
2023-01-20 23:13:16 -0300

    Exercício 3 – Somando dois números

--- PythonExercicios/ex003.py
+++ PythonExercicios/ex003.py
@@ -1,3 +1,4 @@
+# Exercício Python 3: Crie um programa que leia dois números e mostre a soma entre eles.
 number1 = int(input('Digite o primeiro número: '))
 number2 = int(input('Digite o segundo número: '))
 s = number1 + number2
