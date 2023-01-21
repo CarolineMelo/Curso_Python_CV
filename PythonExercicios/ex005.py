87a0bfb98d468c2d2c9866fc9810569e50b1468b
Caroline <2015302@aluno.univesp.br>
2023-01-20 23:17:21 -0300

    Exercício 5 – Antecessor e Sucessor

--- PythonExercicios/ex005.py
+++ PythonExercicios/ex005.py
@@ -1,3 +1,3 @@
 #Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor.
 number = int(input('Digite um número inteiro: '))
-print(' O número digitado é {} .\n Seu antecesso é {} e seu sucessor é {} .'.format(number,number-1,number+1))
+print('O número digitado é {} .\n Seu antecesso é {} e seu sucessor é {} .'.format(number,number-1,number+1))
