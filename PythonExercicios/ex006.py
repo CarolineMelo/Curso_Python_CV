36a940db2e141513b4cf4ca9ec8f3664f73fcb86
Caroline <2015302@aluno.univesp.br>
2023-01-21 23:55:56 -0300

    Exercício 6 – Dobro, Triplo, Raiz Quadrada

--- PythonExercicios/ex006.py
+++ PythonExercicios/ex006.py
@@ -1,4 +1,4 @@
-#Crie um algoritmo que leia um número e mostre o seu dobro,triplo e raiz quadrada.
+'''Python 006: Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada'''
 number = int(input('Digite um número: '))
 print('O número digitado é {}.\nO seu dobro é {}.\nO seu triplo é {}.\nE sua raiz quadrada é {:.2f}' .format(number,number*2,number*3,number **(1/2)))
 
