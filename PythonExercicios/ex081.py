b0ecf19206673a2a850043b45067401d17018141
Caroline <2015302@aluno.univesp.br>
2023-01-21 23:25:26 -0300

    Exercício 81 – Extraindo dados de uma Lista

--- PythonExercicios/ex081.py
+++ PythonExercicios/ex081.py
@@ -1,3 +1,8 @@
+'''Exercício Python 081: Crie um programa que vai ler vários números e colocar em uma lista.Depois disso,mostre:  
+A) Quantos números foram Digitados.  
+B) A lista de valores, ordenada de forma decrescente. 
+C) Se o valor 5 foi digitado e está ou não na lista.'''
+
 lista = []
 while True:
     lista.append(int(input('Digite um valor: ')))
