0730b557887688e8637296dbc17f80bd0c0df9c2
Caroline <2015302@aluno.univesp.br>
2023-02-01 22:54:02 -0300

    Exercício 78 – Maior e Menor valores na Lista

--- PythonExercicios/ex078.py
+++ PythonExercicios/ex078.py
@@ -1,3 +1,4 @@
+# Exercício Python 078: Faça um programa que leia 5 valores numéricos e guarde-os em uma lista. No final, mostre qual foi o maior e o menor valor digitado e as suas respectivas posições na lista.
 number = []
 for cont in range(0,5):
     number.append(int(input(f'Digite um valor para a Posição {cont +1}:')))
@@ -6,5 +7,6 @@ print('-=-'*20)
 print(f'Você digitou os valores {number}')
 max=max(number)
 min=min(number)
-print(f'O maior valor digitado foi {max} na posição {number.index(max)+1}')
-print(f'O menor valor digitado foi {min} na posição {number.index(min)+1}')
+print(f'O maior valor digitado foi {max} na {number.index(max)+1} posição ')
+print(f'O menor valor digitado foi {min} na {number.index(min)+1} posição ')
+
