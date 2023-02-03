1034a3504bd7069ab22d4c544dcd16b026fc7346
Caroline Melo <2015302@aluno.univesp.br>
2023-02-03 19:15:56 -0300

    Exercício 10 – Conversor de Moedas

--- PythonExercicios/ex010.py
+++ PythonExercicios/ex010.py
@@ -1,5 +1,5 @@
+# Exercício Python 10: Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar.
 from ntpath import realpath
-
 
 real = float(input('Quanto dinheiro você tem na carteira? R$'))
 dolar = real / 3.27
