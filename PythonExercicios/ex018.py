eb89c5c6c9ce37cec20c369e686e725f0ebbfaff
Caroline Melo <2015302@aluno.univesp.br>
2023-02-03 19:19:14 -0300

    Exercício 18 – Seno, Cosseno e Tangente

--- PythonExercicios/ex018.py
+++ PythonExercicios/ex018.py
@@ -1,4 +1,8 @@
-'''import math
+'''
+Exercício Python 18: Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno, cosseno e tangente desse ângulo.
+
+
+import math
 angulo = float(input('Digite o ângulo que você deseja:'))
 seno = math.sin(math.radians(angulo))
 cosseno = math.cos(math.radians(angulo))
