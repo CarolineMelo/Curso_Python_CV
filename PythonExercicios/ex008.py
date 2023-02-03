d844c17f1f0baa41c1a953b84ae80c9f916bf32a
Caroline Melo <2015302@aluno.univesp.br>
2023-02-03 19:13:32 -0300

    Exercício 8 – Conversor de Medidas

--- PythonExercicios/ex008.py
+++ PythonExercicios/ex008.py
@@ -1,5 +1,4 @@
-#Escreva um programa que leia um valor em metros 
-# e o exiba convertido em centímetros e milímetros.
+#Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros.
 
 metros = float(input('Digite um valor em metros: '))
 centimetros = metros*100
