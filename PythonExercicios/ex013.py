09e547c0a9cc37704304190cf01804466143423e
Caroline <2015302@aluno.univesp.br>
2023-02-04 22:52:17 -0300

    Exercício 13 – Reajuste Salarial

--- PythonExercicios/ex013.py
+++ PythonExercicios/ex013.py
@@ -1,3 +1,5 @@
+'''Exercício Python 13: Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.'''
+
 salary = float(input('Qual é o salário do Funcioário? R$'))
 salary_readjusted = salary + (salary*15/100)
 print('Um funcionário que ganhava R$ {:.2f}, com 15% de aumento, passa a receber R$ {:.2f}'.format(salary,salary_readjusted))
