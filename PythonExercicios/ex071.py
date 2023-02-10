7068104507486f042413a5648e1a900ce83c939a
Caroline <2015302@aluno.univesp.br>
2023-02-09 23:57:10 -0300

    Exércício 71

---
+++ PythonExercicios/ex071.py
@@ -0,0 +1,14 @@
+''' Exercício Python 071: Crie um programa que simule o funcionamento de um caixa eletrônico. No início, pergunte ao usuário qual será o valor a ser sacado (número inteiro) e o programa vai informar quantas cédulas de cada valor serão entregues. OBS:
+considere que o caixa possui cédulas de R$50, R$20, R$10 e R$1.'''
+
+while True:
+    print('='*20)
+    print('{:^30}'.format('BANCO DEV'))
+    print('='*20)
+    valor = input(int('Qual é o valor que você deseja sacar? R$'))
+
+
+    print('Total de {} cédulas de ')
+    print('Total de {} cédulas de ')
+    print('Total de {} cédulas de ')
+  
