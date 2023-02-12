8070745431420625d401e1f20fa89f98d09d9113
Caroline <2015302@aluno.univesp.br>
2023-02-11 23:27:16 -0300

    Exercício 20 – Sorteando uma ordem na lista

--- PythonExercicios/ex020.py
+++ PythonExercicios/ex020.py
@@ -1,3 +1,4 @@
+'''Exercício Python 20: O mesmo professor do desafio 19 quer sortear a ordem de apresentação de trabalhos dos alunos. Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.'''
 from random import shuffle
 n1 = input('Primeiro aluno: ')
 n2 = input('Segundo aluno: ')
