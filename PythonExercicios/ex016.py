eb5eea95fa80d869e70cbd90a354b1a0d32e2d43
Caroline <2015302@aluno.univesp.br>
2023-02-11 23:16:04 -0300

    Exercício 16 – Quebrando um número

--- PythonExercicios/ex016.py
+++ PythonExercicios/ex016.py
@@ -1,9 +1,7 @@
-#Crie um programa que leia um número real qualquer 
-# pelo teclado e mostre na tela a sua porção inteira
+# Exercício Python 16: Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção Inteira.
 '''from math import floor
 number = float(input('Digite um número: '))
 print('O número {} tem a parte inteira {}.'.format(number,floor(number)))
-
 
 n = float(input('Digite um valor: '))
 print('O número {} tem a parte inteira {}.'.format(n,int(n)))'''
