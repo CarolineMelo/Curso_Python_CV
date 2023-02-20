220caa1fdf382ae7ccc3071fc9499d3ba6888209
Caroline <2015302@aluno.univesp.br>
2023-02-19 23:06:11 -0300

    Exercício 99 – Função que descobre o maior

--- PythonExercicios/ex099.py
+++ PythonExercicios/ex099.py
@@ -1,3 +1,4 @@
+'''Exercício Python 099: Faça um programa que tenha uma função chamada maior(), que receba vários parâmetros com valores inteiros. Seu programa tem que analisar todos os valores e dizer qual deles é o maior.'''
 def maior(*núm):
     if len(núm) == 0:
         print('Nenhum valor foi informado.')
