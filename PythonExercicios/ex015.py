85b50baec795b48121ac44ef58fd9b33b438af75
Caroline <2015302@aluno.univesp.br>
2023-02-20 22:32:02 -0300

    Exercício 15 – Aluguel de Carros

--- PythonExercicios/ex015.py
+++ PythonExercicios/ex015.py
@@ -1,7 +1,6 @@
-#Escreva um programa que pergunta a quantidade de km percorridos por um carro alugado 
-# e a quantidade de dias pelos quais ele foi alugado.
-# Calcule o preço a pagar, sabendo que o carro custa R$60 por dia e 0,15 por Km rodado.
+'''Escreva um programa que pergunta a quantidade de km percorridos por um carro alugado e a quantidade de dias pelos quais ele foi alugado.
+Calcule o preço a pagar, sabendo que o carro custa R$60 por dia e 0,15 por Km rodado.'''
 km = float(input('Qual a quantidade de km percorrido?'))
 dias = int(input('Quantos dias alugados?'))
-custo =(dias *60) + (km * 0.15)
+custo = (dias * 60) + (km * 0.15)
 print('O preço a pagar é {:.2f}'.format(custo))
