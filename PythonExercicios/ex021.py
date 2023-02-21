c7b3f23089d74920b30dd76c6f682ea8765ee1a4
Caroline <2015302@aluno.univesp.br>
2023-02-20 22:34:41 -0300

    Exercício 21 – Tocando um MP3

--- PythonExercicios/ex021.py
+++ PythonExercicios/ex021.py
@@ -1,13 +1,14 @@
+'''Exercício Python 21: Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3.'''
 import pygame
 
 pygame.init()
 pygame.mixer.music.load('ex021.mp3')
+
 pygame.mixer.music.play()
 input()
 pygame.event.wait()
 
-# Faça um programa em Python que abra e reproduza o áudio de um arquivo
-# ================================================================================
+
 # import pygame
 
 # # Inicializando o mixer PyGame
@@ -23,3 +24,17 @@ pygame.event.wait()
 
 # import playsound
 # playsound.playsound('ex021.mp3')
+'''pygame.quit
+import pygame
+
+pygame.init()
+
+try:
+    pygame.mixer.music.load('ex021.mp3')
+    pygame.mixer.music.play()
+except pygame.error:
+    print("Arquivo de áudio 'ex021.mp3' não encontrado.")
+
+input("Aperte enter para sair...")
+pygame.quit()'''
+
