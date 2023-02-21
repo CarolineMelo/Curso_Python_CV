edbfb3141d35df574ce98ccecb42a220a34470a1
Caroline <2015302@aluno.univesp.br>
2023-02-20 22:38:11 -0300

    Exercício 23 – Separando dígitos de um número

--- PythonExercicios/ex023.py
+++ PythonExercicios/ex023.py
@@ -1,12 +1,12 @@
-#Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos dígitos separados.
+'''Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos dígitos separados.'''
 
-# number = int(input('Digite um número: '))
-# n = str(number)
-# print('Analisando o número {}'.format(number))
-# print('Unidade: {}'.format(n[3]))
-# print('Dezena: {}'.format(n[2]))
-# print('Centena: {}'.format(n[1]))
-# print('Milhar: {}'.format(n[0]))
+'''number = int(input('Digite um número: '))
+n = str(number)
+print('Analisando o número {}'.format(number))
+print('Unidade: {}'.format(n[3]))
+print('Dezena: {}'.format(n[2]))
+print('Centena: {}'.format(n[1]))
+print('Milhar: {}'.format(n[0]))'''
 
 n = int(input('Digite um número: '))
 unidade = n // 1 % 10
