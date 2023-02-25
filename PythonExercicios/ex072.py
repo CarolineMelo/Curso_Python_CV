a209e0011720db2c91e68325c9b8b7b3df76808e
Caroline <2015302@aluno.univesp.br>
2023-02-24 23:58:09 -0300

    Exercício 72 – Número por Extenso

--- PythonExercicios/ex072.py
+++ PythonExercicios/ex072.py
@@ -1,4 +1,4 @@
-
+'''Exercício Python 72: Crie um programa que tenha uma dupla totalmente preenchida com uma contagem por extenso, de zero até vinte. Seu programa deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso.'''
 cont = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze',
         'doze', 'treze', 'catorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
 
@@ -8,10 +8,9 @@ while True:
         break
     print('Tente novamente')
 
-print(f' Você digitou o número {cont[number]}')
-    # resp = ' '
-    # while resp not in 'SN':
-    #     resp = str(input('Quer continuar [S/N]: ')).upper().strip()[0]
-    # if resp == 'N':
-    #     break
-       
+    print(f' Você digitou o número {cont[number]}')
+    resp = ' '
+    while resp not in 'SN':
+        resp = str(input('Quer continuar [S/N]: ')).upper().strip()[0]
+    if resp == 'N':
+        break
